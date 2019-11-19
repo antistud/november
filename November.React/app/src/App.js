5a133f9b60ac86dae95e8edee4467f2ba0733741
flothjl <flothjl@hotmail.com>
2019-11-18 23:12:42 -0600

    Added react-router

--- November.React/app/src/App.js
+++ November.React/app/src/App.js
@@ -10,6 +10,7 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import "./App.css";
+import { Switch, Route, Link } from 'react-router-dom'
 
 const ExampleToast = ({ children }) => {
   const [show, toggleShow] = useState(true);
@@ -24,48 +25,70 @@ const ExampleToast = ({ children }) => {
   );
 };
 
+
+
 const NavBarExample = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Tabletop Share</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
+          <NavDropdown title="Menu" id="basic-nav-dropdown">
+            <NavDropdown.Item><Link to='/schedule'>Schedule</Link></NavDropdown.Item>
+            <NavDropdown.Item >
+              <Link to='/roster'>Roster</Link>
             </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Item ><Link to='/'>Home</Link></NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">
               Separated link
             </NavDropdown.Item>
           </NavDropdown>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
 };
+
+function Main() {
+  return (
+    <main>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/roster' component={Roster} />
+        <Route path='/schedule' component={Schedule} />
+      </Switch>
+    </main>
+  );
+}
+const Home = () => (
+  <div>
+    <p>Home</p>
+  </div>
+
+);
+const Roster = () => (
+  <div>
+    <p>Roster</p>
+  </div>
+
+);
+const Schedule = () => (
+  <div>
+    <p>Schedule</p>
+  </div>
+
+);
 const App = () => (
-  <Container className="p-3">
+  <div>
+
     <NavBarExample />
-    <Jumbotron>
-      <h1 className="header">Box Share</h1>
-      <ExampleToast className="toast">
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Jumbotron>
-  </Container>
+    <Main />
+
+
+  </div>
+
 );
 
 export default App;
