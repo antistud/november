b81583320a6b9488442ee0f88093c423702950e1
antistud <jon@t3ch.net>
2019-11-30 10:37:52 -0600

    cors attempt

--- November.Dotnet/Startup.cs
+++ November.Dotnet/Startup.cs
@@ -31,6 +31,12 @@ namespace November.Dotnet
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseCors(x => x
+                      .WithOrigins("*")
+                      .AllowAnyMethod()
+                      .AllowCredentials()
+                      .AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
