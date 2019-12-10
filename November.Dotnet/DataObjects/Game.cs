8187f136079ca3db85d6713daf12aec77065d90c
antistud <jon@t3ch.net>
2019-12-09 20:31:13 -0600

    Updated Game Page

--- November.Dotnet/DataObjects/Game.cs
+++ November.Dotnet/DataObjects/Game.cs
@@ -16,8 +16,8 @@ namespace November.Dotnet
         public string bgg_id { get; set; }
         public bool favorite { get; set; }
         public Object atlas { get; set; }
-        public GamePlay play { get; set; }
-        public GameRequest request { get; set; }
+        public List<GamePlay> play { get; set; }
+        public List<GameRequest> request { get; set; }
 
     }
     public class UserGamePost
