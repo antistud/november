31edbdb82f6e489fd70381898df912c7b41efeed
Jon Rhoades <jrhoades@Jons-MacBook-Pro.local>
2019-12-10 19:16:45 -0600

    changes page and theme

--- November.React/app/src/components/pages/GamePage.js
+++ November.React/app/src/components/pages/GamePage.js
@@ -1,36 +1,19 @@
-import React, { Component } from "react";
+import React, { Component,useEffect,useState } from "react";
 import { useRouteMatch, useParams } from "react-router-dom";
 import Game from "../../services/game";
 
-export class GamePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      game: {
-        _id: "",
-        atlas: {
-          id: "",
-          name: ""
-        }
-      }
-    };
-  }
-
-  componentDidMount() {
-    const {
-      match: { params }
-    } = this.props;
-    console.log(params.gameId);
-    this.gameId = params.gameId;
-    Game.getGameDetails(this.gameId, true).then(res => {
-      this.game = res.data;
-      console.log("this.game", res.data);
-      this.setState({ game: res.data });
-      console.log("state", this.state);
+  function GamePage(props) {
+    const [game, setGame] = useState();
+    let { gameId } = useParams();
+    useEffect(async () => {
+       Game.getGameDetails(gameId).then(res => {
+        setGame(res.data);    
     });
-  }
 
-  createRequestsList = () => {
+    }, []);
+
+  
+  function createRequestsList() {
     let table = [];
     for (let j = 0; j < 5; j++) {
       table.push(<div>Request: {j}</div>);
@@ -38,25 +21,76 @@ export class GamePage extends Component {
     return table;
   };
 
-  render() {
-    return (
-      <div>
-        <div className="card">
+if(game){
+  return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-7">
+           <div className="card gameDetails">
           <img
-            src={this.state.game.atlas.image_url}
+            src={game.atlas.image_url}
             className="card-img-top"
           ></img>
           <div className="card-body">
-            <h5 className="card-title">{this.state.game.atlas.name}</h5>
+            <h5 className="card-title">{game.atlas.name}</h5>
             <p className="card-text">
-              Publisher: {this.state.game.atlas.primary_publisher}
+              Publisher: {game.atlas.primary_publisher}
             </p>
           </div>
+          <div className="card-footer text-right">
+          More Details
+          </div>
+        </div> 
+        </div>        
+        <div className="col-sm-5">
+              <div className="row">
+          <div className="col-sm">
+<a href="#" className="btn btn-block btn-primary mt-2"><i className="fa fa-dice"></i><br />Record Play</a>
+          </div>
+      
+<div className="col-sm">
+<a href="#" className="btn btn-block btn-primary mt-2"> <i className="fa fa-share-square"></i><br /> Add Request</a>
+          </div> 
+          
+          <div className="col-sm">
+<a href="#" className="btn btn-block btn-danger mt-2"><i className="fa fa-trash"></i><br />Remove</a>
+          </div>
+          </div>
+
+        <div className="card ">
+        <div className="card-header">
+          Recent Plays
         </div>
-        {this.createRequestsList()}
+        <div className="card-body">
+        {createRequestsList()}
+        </div>
+        <div className="card-footer text-right">
+          View All
+          </div>
+        </div>
+
+        <div className="card">
+        <div className="card-header">
+          Open Requests
+        </div>
+        <div className="card-body">
+        {createRequestsList()}
+        </div>
+        <div className="card-footer text-right">
+        View All
+          </div>
+        </div>
+        </div>
+        </div>
+
       </div>
     );
-  }
+
+}else{
+  return(
+    <div className="loading">Loading...</div>
+  )
+}
 }
 
 export default GamePage;
