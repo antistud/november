5bc32066717fe68740f6ef84e97d8e39dbf9d2a9
Jon Rhoades <jrhoades@Jons-MacBook-Pro.local>
2019-12-12 00:12:16 -0600

    disable zoom?

--- November.React/app/src/App.js
+++ November.React/app/src/App.js
@@ -109,7 +109,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+       <div className="App">
           <AppNavbar
             username={
               localStorage.getItem("profile")
