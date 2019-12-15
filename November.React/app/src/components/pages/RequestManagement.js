8aa2629aea03f6c07d8217b4e4b318b220db2f15
Josh Floth <thadiusbacon@gmail.com>
2019-12-14 21:29:12 -0600

    Saving Request Management progress

--- November.React/app/src/components/pages/RequestManagement.js
+++ November.React/app/src/components/pages/RequestManagement.js
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import Request from '../../services/request'
-
+import React, { useEffect, useState } from "react";
+import Request from "../../services/request";
+import { CardColumns, Card } from "react-bootstrap";
 function RequestManagement() {
-    let [state, setState] = useState();
+  let [state, setState] = useState();
 
-    useEffect(() => {
-        console.log(state)
+  useEffect(() => {
+    console.log(state);
 
-        Request.getRequests().then(res => {
-            console.log(res);
-            setState({ requests: res.data });
-        })
-    }, []);
-    if (state) {
-        let games = state.requests.map(request => (
-            <div> {request.user_id}</div>
-        ))
+    Request.getRequests().then(res => {
+      console.log(res);
+      setState({ requests: res.data });
+    });
+  }, []);
+  if (state) {
+    let games = state.requests.map(request => (
+      <Card>
+        <Card.Img
+          className="card-holder-img"
+          variant="top"
+          src="holder.js/100px160/"
+        ></Card.Img>
+        <Card.Body>
+          <Card.Title>{request.user_id}</Card.Title>
+          <Card.Text>Text about the request</Card.Text>
+        </Card.Body>
+      </Card>
+    ));
 
-
-        return (
-            <div>
-                {games}
-            </div>
-        )
-    }
-    else { return (<div>Loading...</div>) }
-
+    return <CardColumns>{games}</CardColumns>;
+  } else {
+    return <div>Loading...</div>;
+  }
 }
 
-export default RequestManagement
+export default RequestManagement;
