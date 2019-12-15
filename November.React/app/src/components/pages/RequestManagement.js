4c1380b6d2de31aa07a0432c0f242e3f3dabb1ee
flothjl <flothjl@hotmail.com>
2019-12-15 10:09:13 -0600

    saving progress

--- November.React/app/src/components/pages/RequestManagement.js
+++ November.React/app/src/components/pages/RequestManagement.js
@@ -1,28 +1,39 @@
 import React, { useEffect, useState } from "react";
 import Request from "../../services/request";
 import { CardColumns, Card } from "react-bootstrap";
+import { useHistory } from "react-router-dom"
+import Game from "../../services/game"
 function RequestManagement() {
-  let [state, setState] = useState();
-
+  let [state, setState] = useState({ requests: [] });
+  let history = useHistory()
+  function onClick(game_id) {
+    console.log(game_id)
+    history.push('/Game/' + game_id)
+  }
   useEffect(() => {
-    console.log(state);
 
+    console.log(state);
     Request.getRequests().then(res => {
       console.log(res);
-      setState({ requests: res.data });
+      let resdata = res.data.map(req => (
+        Game.getGameDetails(req.game_id).then(res => { if (res.data.atlas) { console.log(res); req.atlas_image = res.data.atlas.images.medium; } })
+      ))
+      setState({ requests: resdata })
     });
   }, []);
   if (state) {
+    console.log(state)
     let games = state.requests.map(request => (
-      <Card>
+      <Card onClick={() => onClick(request.game_id)}>
         <Card.Img
           className="card-holder-img"
           variant="top"
-          src="holder.js/100px160/"
+          // src={request.atlas_image}
+          src='holderjs'
         ></Card.Img>
         <Card.Body>
           <Card.Title>{request.user_id}</Card.Title>
-          <Card.Text>Text about the request</Card.Text>
+          <Card.Text>{request.game_id}</Card.Text>
         </Card.Body>
       </Card>
     ));
