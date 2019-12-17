67dda1a8d9092253cba4403f39687244c4c3a859
flothjl <flothjl@hotmail.com>
2019-12-16 22:49:22 -0600

    request tracking page updates

--- November.React/app/src/components/pages/RequestManagement.js
+++ November.React/app/src/components/pages/RequestManagement.js
@@ -5,6 +5,8 @@ import { useHistory } from "react-router-dom";
 import Moment from "react-moment";
 import Game from "../../services/game";
 import Requests from "../../services/request";
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
 function RequestManagement() {
   let [state, setState] = useState();
   let history = useHistory();
@@ -24,14 +26,49 @@ function RequestManagement() {
       }
     });
   }, []);
+
+  function cardFooter(request) {
+    if (request.send_sent != "0001-01-01T00:00:00Z") {
+      return (
+        <Card.Footer>
+          <small className="text-muted"><Moment fromNow>{request.send_sent}</Moment></small>
+        </Card.Footer>
+      )
+    }
+  }
+  function getGameState(request) {
+    let percent = 0
+    let status = ''
+    if (request.return_recieved !== "0001-01-01T00:00:00Z") {
+      percent = 100
+      status = "Game Returned"
+    }
+    else if (request.return_sent !== "0001-01-01T00:00:00Z") {
+      percent = 67
+      status = request.user.name + " has sent your game back"
+    }
+    else if (request.send_recieved !== "0001-01-01T00:00:00Z") {
+      percent = 33
+      status = request.user.name + " recieved your game."
+    }
+    else if (request.send_sent !== "0001-01-01T00:00:00Z") {
+      percent = 0
+      status = "Game sent."
+    }
+    else {
+      percent = 0
+      status = request.user.name + " is waiting for you to send this game."
+    }
+    return { percent: percent, status: status }
+
+  }
   if (state) {
     console.log(state);
     let games = state.requests.map(request => {
       if (
-        request.game !== null &&
-        request.user.user_id !==
-          JSON.parse(localStorage.getItem("profile")).user_id
+        request.game !== null
       ) {
+        let status = getGameState(request)
         return (
           <Card
             className="gameDetails"
@@ -48,12 +85,56 @@ function RequestManagement() {
               <Card.Title>
                 {request.user.name} requested {request.game.data.name}
               </Card.Title>
-              <Card.Text>{request.game_id}</Card.Text>
+              <Card.Text>
+                <ProgressBar
+                  filledBackground="linear-gradient(to right, #7abaa9, #00BC8B)"
+                  percent={status.percent}
+                >
+                  <Step transition="scale">
+                    {({ accomplished, index }) => (
+                      <div
+                        className={`transitionStep ${accomplished ? "accomplished" : null}`}
+                      >
+                        <i className="fa fa-circle" style={{ color: '#00ad7f' }}></i>
+                      </div>
+                    )}
+                  </Step>
+                  <Step transition="scale">
+                    {({ accomplished, index }) => (
+                      <div
+                        className={`transitionStep ${accomplished ? "accomplished" : null}`}
+                      >
+                        <i className="fa fa-circle" style={{ color: '#00ad7f' }}></i>
+                      </div>
+                    )}
+                  </Step>
+                  <Step transition="scale">
+                    {({ accomplished, index }) => (
+                      <div
+                        className={`transitionStep ${accomplished ? "accomplished" : null}`}
+                      >
+                        <i className="fa fa-circle" style={{ color: '#00ad7f' }}></i>
+                      </div>
+                    )}
+                  </Step>
+                  <Step transition="scale">
+                    {({ accomplished, index }) => (
+                      <div
+                        className={`transitionStep ${accomplished ? "accomplished" : null}`}
+                      >
+
+                        <i className="fa fa-circle" style={{ color: '#00ad7f' }}></i>
+                      </div>
+                    )}
+                  </Step>
+
+                </ProgressBar>
+                <br />
+                Status: {status.status}
+              </Card.Text>
             </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
+            {cardFooter(request)}
+          </Card >
         );
       }
     });
