de0ee9308c5f58b821463ee23d7f1a7482cf301f
antistud-ac <jrhoades@auroracoop.com>
2019-12-21 10:15:42 -0600

    added to lower to create user

--- November.Dotnet/Controllers/Auth.cs
+++ November.Dotnet/Controllers/Auth.cs
@@ -123,7 +123,7 @@ namespace November.Dotnet.Controllers
                 {
                     try
                     {
-                        var docs = host.c_auth.Find(x => x.username == body.email).ToList().First();
+                        var docs = host.c_auth.Find(x => x.username.ToLower() == body.email.ToLower()).ToList().First();
                         return Ok("User Already Exists");
                     }
                     catch
@@ -131,13 +131,13 @@ namespace November.Dotnet.Controllers
                         var profile = Profile();
                         var id = ObjectId.GenerateNewId().ToString();
                         var password = randompassword();
-                        host.c_auth.InsertOneAsync(new User { _id = id, username = body.email, hash = UserPassword.HashPassword(password) });
-                        host.c_profile.InsertOneAsync(new UserProfile { user_id = id, username = randomUsername(), email = body.email });
+                        host.c_auth.InsertOneAsync(new User { _id = id, username = body.email.ToLower(), hash = UserPassword.HashPassword(password) });
+                        host.c_profile.InsertOneAsync(new UserProfile { user_id = id, username = randomUsername(), email = body.email.ToLower() });
                         host.c_friend.InsertOneAsync(new UserFriend { user_id = id, friend_id = profile.user_id });
                         var sg_subject = "This is going to be Fun!!!";
-                        var sg_to = new EmailAddress(body.email);
-                        var sg_plainTextContent = "You have been invited to BoxShare username: " + body.email + " password: " + password + " URL: http://app.garishgames.com";
-                        var sg_htmlContent = $"<strong>You have been invited to BoxShare.</strong><br><br>username: " + body.email + "<br>password: " + password + " <br><br> <a href='http://app.garishgames.com'>Go Now</a>";
+                        var sg_to = new EmailAddress(body.email.ToLower());
+                        var sg_plainTextContent = "You have been invited to BoxShare username: " + body.email.ToLower() + " password: " + password + " URL: http://app.garishgames.com";
+                        var sg_htmlContent = $"<strong>You have been invited to BoxShare.</strong><br><br>username: " + body.email.ToLower() + "<br>password: " + password + " <br><br> <a href='http://app.garishgames.com'>Go Now</a>";
                         var sg_msg = MailHelper.CreateSingleEmail(host.sg_from, sg_to, sg_subject, sg_plainTextContent, sg_htmlContent);
                         var sg_response = host.sg_client.SendEmailAsync(sg_msg);
                         sg_response.ToJson();
